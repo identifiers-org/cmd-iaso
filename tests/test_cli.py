892fc2b2787699eadbe2de478b975b9a0b56bd88
Moritz Langenstein <moritz.langenstein17@imperial.ac.uk>
2020-05-14 12:25:53 +0000

    (ml5717) Traceback Windows specific CLI error

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,5 @@
+import traceback
+
 from click.testing import CliRunner
 
 from iaso.cli import cli
@@ -6,7 +8,13 @@ from iaso.cli import cli
 class TestCLI:
     def test_environment(self):
         runner = CliRunner()
-        result = runner.invoke(cli, args=["environment"], prog_name="cmd-iaso")
+        result = runner.invoke(
+            cli,
+            args=["environment"],
+            prog_name="cmd-iaso",
+            color=True,
+            catch_exceptions=False,
+        )
 
         assert result.exit_code == 0
 
