14fd7e8d37bf407a96fc68845033eeb8b8406949
Moritz Langenstein <moritz.langenstein17@imperial.ac.uk>
2020-05-14 11:52:17 +0000

    (ml5717) Added simple cli system test

---
+++ tests/test_cli.py
@@ -0,0 +1,16 @@
+from click.testing import CliRunner
+
+from iaso.cli import cli
+
+
+class TestCLI:
+    def test_environment(self):
+        runner = CliRunner()
+        result = runner.invoke(cli, args=["environment"], prog_name="cmd-iaso")
+
+        assert result.exit_code == 0
+
+        print()
+        print(" TESTING ENVIRONMENT ".center(80, "="))
+        print(result.output, end="")
+        print(" TESTING ENVIRONMENT ".center(80, "="))
