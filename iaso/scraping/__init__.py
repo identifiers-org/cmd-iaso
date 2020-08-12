55ee07b4ff9f96c2300bbeb9d1cb4946a47eac02
Moritz Langenstein <moritz.langenstein17@imperial.ac.uk>
2020-08-12 19:18:27 +0000

    (MoritzLangenstein) Removed unnecessary signal handler reset

--- iaso/scraping/__init__.py
+++ iaso/scraping/__init__.py
@@ -1,7 +1,6 @@
 import asyncio
 import logging
 import multiprocessing as mp
-import signal
 import warnings
 
 from pyppeteer.chromium_downloader import check_chromium, download_chromium
@@ -12,8 +11,6 @@ from .pool import scrape_resources_pool
 
 
 async def scrape_resources(jobs, dump, proxy_address, chrome, workers, timeout):
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-
     if chrome is None:
         if not check_chromium():
             patch_pyppeteer()
